599369230fc9a83ab7d0e712573ef2f292d99161
swamm-28 <swaminikulkarni2007@gmail.com>
2025-08-14 15:14:08 +0000

    123

--- poin.c
+++ poin.c
@@ -1,12 +1,12 @@
-#include<stdio.h>    
-int main()
-{ 
-int number=50;      
-int *p;        
-p=&number; 
-
-printf("Address of p variable is %x \n", p);   
-printf("Value of p variable is %d \n", *p);  
-   
-return 0;    
+#include<stdio.h>  
+void main ()  
+{  
+    int arr[5] = {1, 2, 3, 4, 5};  
+    int *p = arr;  
+    int i;  
+    printf("printing array elements...\n");  
+    for(i = 0; i< 5; i++)  
+    {  
+        printf("%d  ",*(p+i));  
+    }  
 }
