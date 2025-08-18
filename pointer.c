c669881886b4885ccad247dbe9fa20174fba0e09
swamm-28 <swaminikulkarni2007@gmail.com>
2025-08-18 14:44:24 +0000

    18/8

--- pointer.c
+++ pointer.c
@@ -1,15 +1,12 @@
 #include<stdio.h>  
 void main ()  
 {  
-    int a = 10;  
-    int *p;  
-    int **pp; 
-
-    p = &a; 
-    pp = &p;
-    
-    printf("address of a: %x\n",p); 
-    printf("address of p: %x\n",pp); 
-    printf("value stored at p: %d\n",*p); 
-    printf("value stored at pp: %d\n",**pp);
-}
+    int arr[5] = {1, 2, 3, 4, 5};  
+    int *p = arr;  
+    int i;  
+    printf("printing array elements...\n");  
+    for(i = 0; i< 5; i++)  
+    {  
+        printf("%d  ",*(p+i));  
+    }  
+}  
