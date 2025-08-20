19bef398370cfe35df63064be2ae2b700cd32ed3
swamm-28 <swaminikulkarni2007@gmail.com>
2025-08-20 15:24:40 +0000

    20/8

---
+++ stringg.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main ()
+{
+    char s[20];
+     printf("Enter the string: ");    
+    scanf("%s",s);    
+    printf("You entered %s",s);  
+
+}
---
+++ strlen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+#include <string.h>  
+int main() 
+{     
+    char string[] = "Hi how are you";  
+    int a=strlen(string);
+    
+    printf("Length of the string is: %d\n", a);  
+     
+}  
