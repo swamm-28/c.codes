88cb5d3cffcc31f29de47818f8715587f37c16c6
swamm-28 <swaminikulkarni2007@gmail.com>
2025-08-20 15:00:45 +0000

    8/20

---
+++ str.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main ()
+{
+      char ch[11]={ 't', 'p', 'o', 'i', 'n', 't', 't', 'e', 'c', 'h', '\0'};      
+      char ch2[11];
+
+           printf("Char Array Value is: %s\n", ch);      
+   printf("String Literal Value is: %s\n", ch2);   
+    
+}
