e2294891194f73795f861b5f6802358e57e9484c
swamm-28 <swaminikulkarni2007@gmail.com>
2025-08-21 15:00:40 +0000

    8.30
    8.30\

--- exp.c
+++ exp.c
@@ -1,1 +1,11 @@
-exp.c
+#include <stdio.h>  
+#include <math.h>  
+  
+int main() {  
+double base = 2.0;  
+double exponent = 3.0;  
+double result = pow(base, exponent); 
+ 
+printf("%.2f raised to the power of %.2f is %.2f\n", base, exponent, result);  
+
+}  
